d262af80057880d03c73d655e5e066bc0417cd71
Alain Brenzikofer <alain@integritee.network>
2024-04-12 10:14:40 +0200

    clean up L1 Balance display

--- src/Balances.js
+++ src/Balances.js
@@ -1,87 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { Table, Grid, Button, Label } from 'semantic-ui-react'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
-import { useSubstrateState } from './substrate-lib'
-
+import React, {useEffect, useState} from 'react'
+import {useSubstrateState} from './substrate-lib'
+import { formatBalance } from '@polkadot/util';
+const acctAddr = acct => (acct ? acct.address : '')
 export default function Main(props) {
-  const { api, keyring } = useSubstrateState()
-  const accounts = keyring.getPairs()
-  const [balances, setBalances] = useState({})
+    const {api, currentAccount} = useSubstrateState()
+    const [accountBalance, setAccountBalance] = useState('none')
 
-  useEffect(() => {
-    const addresses = keyring.getPairs().map(account => account.address)
-    let unsubscribeAll = null
+    formatBalance.setDefaults({
+        decimals: 10,
+        unit: 'PAS'
+    });
 
-    api.query.system.account
-      .multi(addresses, balances => {
-        const balancesMap = addresses.reduce(
-          (acc, address, index) => ({
-            ...acc,
-            [address]: balances[index].data.free.toHuman(),
-          }),
-          {}
-        )
-        setBalances(balancesMap)
-      })
-      .then(unsub => {
-        unsubscribeAll = unsub
-      })
-      .catch(console.error)
+// When account address changes, update subscriptions
+    useEffect(() => {
+        let unsubscribe
 
-    return () => unsubscribeAll && unsubscribeAll()
-  }, [api, keyring, setBalances])
+        // If the user has selected an address, create a new subscription
+        currentAccount &&
+        api.query.system
+            .account(acctAddr(currentAccount), balance =>
+                setAccountBalance(formatBalance(balance.data.free))
+            )
+            .then(unsub => (unsubscribe = unsub))
+            .catch(console.error)
 
-  return (
-    <Grid.Column>
-      <h1>Balances</h1>
-      {accounts.length === 0 ? (
-        <Label basic color="yellow">
-          No accounts to be shown
-        </Label>
-      ) : (
-        <Table celled striped size="small">
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell width={3} textAlign="right">
-                <strong>Name</strong>
-              </Table.Cell>
-              <Table.Cell width={10}>
-                <strong>Address</strong>
-              </Table.Cell>
-              <Table.Cell width={3}>
-                <strong>Balance</strong>
-              </Table.Cell>
-            </Table.Row>
-            {accounts.map(account => (
-              <Table.Row key={account.address}>
-                <Table.Cell width={3} textAlign="right">
-                  {account.meta.name}
-                </Table.Cell>
-                <Table.Cell width={10}>
-                  <span style={{ display: 'inline-block', minWidth: '31em' }}>
-                    {account.address}
-                  </span>
-                  <CopyToClipboard text={account.address}>
-                    <Button
-                      basic
-                      circular
-                      compact
-                      size="mini"
-                      color="blue"
-                      icon="copy outline"
-                    />
-                  </CopyToClipboard>
-                </Table.Cell>
-                <Table.Cell width={3}>
-                  {balances &&
-                    balances[account.address] &&
-                    balances[account.address]}
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      )}
-    </Grid.Column>
-  )
+        return () => unsubscribe && unsubscribe()
+    }, [api, currentAccount])
+
+    return (
+      <div>
+          My L1 Balance: {accountBalance}
+      </div>
+    )
 }
