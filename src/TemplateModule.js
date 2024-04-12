0dfbba0f3d7f88db2e6a25c7630d01c85d7c5996
Alain Brenzikofer <alain@integritee.network>
2024-04-12 12:18:34 +0200

    show L2 balance works

--- src/TemplateModule.js
+++ src/TemplateModule.js
@@ -1,18 +1,24 @@
 import React, {useEffect, useState} from 'react'
-import {Form, Input, Grid, Card, Statistic} from 'semantic-ui-react'
+import {Form, Input, Grid} from 'semantic-ui-react'
 
 import {useSubstrate, useSubstrateState} from './substrate-lib'
 import {TxButton} from './substrate-lib/components'
 import {IntegriteeWorker} from '@encointer/worker-api';
-import {Keyring} from "@polkadot/keyring";
+//import {Keyring} from "@polkadot/keyring";
+import { formatBalance } from '@polkadot/util';
 
 function Main(props) {
-  const {setVaultAccount, state: {api, vaultAccount}} = useSubstrate()
+  const {setVaultAccount, state: {api, currentAccount}} = useSubstrate()
   // The transaction submission status
   const [status, setStatus] = useState('')
   // The currently stored value
-  const [currentValue, setCurrentValue] = useState(0)
   const [formValue, setFormValue] = useState(0)
+  const [incogniteeBalance, setIncogniteeBalance] = useState(0)
+
+    formatBalance.setDefaults({
+        decimals: 10,
+        unit: 'iPAS'
+    });
 
   useEffect(() => {
     const worker = new IntegriteeWorker('wss://scv1.paseo.api.incognitee.io:443', {
@@ -25,67 +31,51 @@ function Main(props) {
       console.log('Vault: ')
       console.log(sk[0])
     });
-    let keyring = new Keyring({type: "sr25519"});
-    let alice = keyring.addFromUri('//Alice', {name: 'Alice default'});
-    let bob = keyring.addFromUri('//Bob', {name: 'Alice default'});
-    // works
     const shard = '5wePd1LYa5M49ghwgZXs55cepKbJKhj5xfzQGfPeMS7c';
-    const mrenclave = '7RuM6U4DLEtrTnVntDjDPBCAN4LbCGRpnmcTYUGhLqc7';
+
+    // works
+
     //
     // // works: get balance of an account for a shard
-    // worker.getBalance(alice, shard)
-    //     .then((balance) => console.log(`Alice balance: ${balance}`));
+    worker.getBalance(currentAccount, shard)
+         .then((balance) => {
+           setIncogniteeBalance(formatBalance(balance))
+           console.log(`current account balance L2: ${balance}`)
+         });
     //
     // // works: get the nonce of an account for a shard.
     // // We don't actually need to use this. `trustedBalanceTransfer` uses this internally.
     //   worker.getNonce(alice, shard)
     //     .then((nonce) => console.log(`Alice balance: ${nonce}`));
 
-    try {
-      // this does only call `author_submit`, so we can only know if the trusted call is valid, but we
-      // can't know here if the trusted call has been executed without an error.
-      worker.trustedBalanceTransfer(
-        alice,
-        shard,
-        mrenclave,
-        alice.address,
-        bob.address,
-        1100000000000
-      ).then((hash) => console.log(`trustedOperationHash: ${hash}`));
 
-    } catch (error) {
-      console.log(`Error submitting the trusted operation: ${error}`)
-    }
 
-    let unsubscribe
-    api.query.system
-      .palletVersion(newValue => {
-        // The storage value is an Option<u32>
-        // So we have to check whether it is None first
-        // There is also unwrapOr
-        if (newValue.isNone) {
-          setCurrentValue('<None>')
-        } else {
-          // setCurrentValue(newValue.unwrap().toNumber())
-        }
-      })
-      .then(unsub => {
-        unsubscribe = unsub
-      })
-      .catch(console.error)
+    // try {
+    //  const mrenclave = '7RuM6U4DLEtrTnVntDjDPBCAN4LbCGRpnmcTYUGhLqc7';
+    // let keyring = new Keyring({type: "sr25519"});
+    // let alice = keyring.addFromUri('//Alice', {name: 'Alice default'});
+    // let bob = keyring.addFromUri('//Bob', {name: 'Alice default'});
+    //   // this does only call `author_submit`, so we can only know if the trusted call is valid, but we
+    //   // can't know here if the trusted call has been executed without an error.
+    //   worker.trustedBalanceTransfer(
+    //     alice,
+    //     shard,
+    //     mrenclave,
+    //     alice.address,
+    //     bob.address,
+    //     1100000000000
+    //   ).then((hash) => console.log(`trustedOperationHash: ${hash}`));
+    //
+    // } catch (error) {
+    //   console.log(`Error submitting the trusted operation: ${error}`)
+    // }
 
-    return () => unsubscribe && unsubscribe()
-  }, [api.query.system, api])
+  }, [api.query.system, api, currentAccount, setIncogniteeBalance])
 
   return (
     <Grid.Column width={8}>
       <h1>Incognitee L2</h1>
-      Vault account: {vaultAccount}
-      <Card centered>
-        <Card.Content textAlign="center">
-          <Statistic label="Current Value" value={currentValue}/>
-        </Card.Content>
-      </Card>
+      <p>My L2 balance: {incogniteeBalance}</p>
       <Form>
         <Form.Field>
           <Input
